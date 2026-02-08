6954ec472ca38cf3a8b16d0329cb3c6cdb65c485
lakrasumit <lakrasumit633@gmail.com>
2026-02-08 15:31:58 +0530

    8/2/26

---
+++ 2166B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << (a >= b * c || b >= a ? 1 : 2) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ 2194A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a - (a / b) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
